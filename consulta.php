57526d9f3c279024a7bcbf6444e60ff6bc0438b0
Lívia Ribeiro <liviaribep@gmail.com>
2024-08-01 17:08:52 -0300

    01/08

---
+++ consulta.php
@@ -0,0 +1,14 @@
+<?php
+    include "conecta.php";
+    $sql ="SELECT id, nome, senha, email FROM usuario";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while($registro = mysqli_fetch_assoc($resultado)){
+            echo "Id: " .  $registro["id"]. " - Nome: " . $registro["nome"].
+            " " . $registro["senha"]. " - Email: " . $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado.";
+    }
+?>
