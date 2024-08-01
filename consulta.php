fce6e60de57715077865a43f9e4acb1c8ae4141b
Lívia Ribeiro <liviaribep@gmail.com>
2024-08-01 17:43:23 -0300

    01/08

--- consulta.php
+++ consulta.php
@@ -54,7 +54,7 @@
     echo "<br>";
     if (mysqli_num_rows($resultado) > 0){
         while($registro = mysqli_fetch_assoc($resultado)){
-            echo "Id: " .  $registro["id"]. " - Nome: " . $registro["nome"].
+            echo " - Nome: " . $registro["nome"].
             " " . $registro["senha"]. " - Email: " . $registro["email"]. "<br>";
         }
     } else {
