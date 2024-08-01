f93ad755ab88fe35744f642d1ca027ded3665073
Lívia Ribeiro <liviaribep@gmail.com>
2024-08-01 17:41:24 -0300

    01/08

--- consulta.php
+++ consulta.php
@@ -3,6 +3,55 @@
     $sql ="SELECT id, nome, senha, email FROM usuario";
     $resultado = mysqli_query($conexao, $sql);
     echo "<br>";
+    echo "<br>";
+    echo "<b>-- Todos os registros</b>";
+    echo "<br>";
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while($registro = mysqli_fetch_assoc($resultado)){
+            echo "Id: " .  $registro["id"]. " - Nome: " . $registro["nome"].
+            " " . $registro["senha"]. " - Email: " . $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado.";
+    }
+
+    echo "<br>";
+    echo "<b>-- Nomes que começam com a letra 'a'</b>";
+    echo "<br>";
+    $sql ="SELECT id, nome, senha, email FROM usuario WHERE nome LIKE 'a%'";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while($registro = mysqli_fetch_assoc($resultado)){
+            echo "Id: " .  $registro["id"]. " - Nome: " . $registro["nome"].
+            " " . $registro["senha"]. " - Email: " . $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado.";
+    }
+
+    echo "<br>";
+    echo "<b>-- Ordenar registros por email em ordem alfabética</b>";
+    echo "<br>";
+    $sql ="SELECT id, nome, senha, email FROM usuario ORDER BY email";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while($registro = mysqli_fetch_assoc($resultado)){
+            echo "Id: " .  $registro["id"]. " - Nome: " . $registro["nome"].
+            " " . $registro["senha"]. " - Email: " . $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado.";
+    }
+
+    echo "<br>";
+    echo "<b>-- Verificar se existe usuário = 'Livia' com senha = '3012' e mostrar seu email</b>";
+    echo "<br>";
+    $sql ="SELECT id, nome, senha, email FROM usuario WHERE nome = 'Livia' and senha = '3012'";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
     if (mysqli_num_rows($resultado) > 0){
         while($registro = mysqli_fetch_assoc($resultado)){
             echo "Id: " .  $registro["id"]. " - Nome: " . $registro["nome"].
